518aaea105796cfadb94b82ce10504f089396c31
Nam Luu <nam.luu@gmail.com>
2015-04-27 17:57:53 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   root 'videos#index'
 
+  match '/signup' => 'users#new', via: [:get]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
