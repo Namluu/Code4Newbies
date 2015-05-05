4c851fc9d9f043b188e973eb134502ac3402858b
nam.luu <nam.luu@bluecomgroup.com>
2015-05-05 17:32:40 +0700

    add password has not encrypt yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :name, :presence => true, :length => {:maximum => 10}
 	validates :email, :presence => true, :format => {:with => email_regex}, :uniqueness => true
+	validates :password, :presence => true, :confirmation => true, :length => {:within => 6..40}
 end
