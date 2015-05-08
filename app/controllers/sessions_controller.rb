af6d34175675a71ec202397f3d489d4b4921130d
nam.luu <nam.luu@bluecomgroup.com>
2015-05-08 16:30:59 +0700

    update log in, log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,11 +10,14 @@ class SessionsController < ApplicationController
   		render 'new'
   	else
   		flash[:success] = "Yes"
+      sign_in user
   		redirect_to user
   	end
   end
 
   def destroy
+    sign_out
+    redirect_to root_path
   end
 
   private
